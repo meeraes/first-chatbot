694e05a0be7ad0a2edbbb301cd93eae5ebea2ad7
meeraes <164362247+meeraes@users.noreply.github.com>
2024-11-26 18:59:42 +0530

    APIK from Streamlit Secrets

--- bot1/Chatbot/bot1.py
+++ bot1/Chatbot/bot1.py
@@ -9,7 +9,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-os.environ['GROQ_API_KEY'] = os.getenv("GROQ_API_KEY")
+os.environ['GROQ_API_KEY'] = st.secrets("GROQ_API_KEY") # Changed os.getenv to get the API Key to st.secrets for the Streamlit app
 
 # Defining Prompt Template
 prompt = ChatPromptTemplate.from_messages(
